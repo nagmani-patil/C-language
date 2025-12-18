2e8a7fbd26260d65fa887a40c1a423aed9d51dec
@Nagmanicodes <patilnagmani1@gmail.com>
2025-12-18 07:30:01 +0530

    Add files via upload

---
+++ a3program7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if(age < 12)
+        printf("Child\n");
+    else if(age >= 12 && age <= 19)
+        printf("Teenager\n");
+    else if(age >= 20 && age <= 59)
+        printf("Adult\n");
+    else
+        printf("Senior\n");
+
+    return 0;
+}
